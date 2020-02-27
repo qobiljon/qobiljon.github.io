ca80ec7c19e999d9410b0da0d6f0513a0bdf4610
Qobiljon <qobiljon.toshnazarov@gmail.com>
2020-02-27 21:36:40 +0900

    awards (certificates) added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let modal, modalImg, captionText;
+
+document.addEventListener("DOMContentLoaded", () => {
+    modal = document.getElementById("myModal");
+    modalImg = document.getElementById("img01");
+    captionText = document.getElementById("caption");
+});
+
 function copyEmailToClipboard() {
     navigator.clipboard.writeText("kobiljon@nsl.inha.ac.kr").then(function () {
         showToast("Email address has been copied to clipboard successfully!");
@@ -13,4 +21,14 @@ function showToast(message) {
     setTimeout(function () {
         toast.className = toast.className.replace("show", "");
     }, 3000);
+}
+
+function openImageModalView(element) {
+    modal.style.display = "block";
+    modalImg.src = element.src;
+    captionText.innerHTML = element.alt;
+}
+
+function closeImageModalView() {
+    modal.style.display = "none";
 }
